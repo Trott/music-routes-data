30d782dabb63bdf1cded7f676c137e226b59bcc4
Rich Trott <rtrott@gmail.com>
2013-12-26 21:31:16 -0800

    revise lib and tests

--- spec/data.spec.js
+++ spec/data.spec.js
@@ -1,11 +1,46 @@
 // Specs are run via Grunt: grunt jasmine_node
 
 var path = require('path');
-var data = require('../lib/data.js');
+var Data = require('../lib/Data.js');
+var data;
 
-describe('data.load()', function() {
-  it('should return true if data loaded from specified directory', function() {
-    var dataDir = path.resolve('./spec/fixture/data');
-    expect(data.load(dataDir)).toBe(true);
+describe('Constructor', function () {
+
+  it('should load from specified directory', function() {
+    var dataDir = path.resolve('spec/fixture/data');
+    data = new Data({dataDir: dataDir});
+    expect(data.getTracks()).toEqual([{"id":"1", "titles":["That's All Right"]}, {"id":"2", "titles":["Blue Moon of Kentucky"]}]);
   });
+
+  it('should throw an exception if data directory does not exist', function () {
+    var dataDir = path.resolve('bad/path');
+    expect(function () {data = new Data({dataDir: dataDir});}).toThrow();
+  });
+
+  it('should load from the data directory if no directory is specified', function () {
+    var dataDir = path.resolve('data');
+    var dataWithArgument = new Data({dataDir: dataDir});
+    var dataWithoutArgument = new Data();
+    expect(dataWithArgument.getTracks()).toEqual(dataWithoutArgument.getTracks());
+  });
+
+});
+
+describe ('non-Constructor tests', function () {
+
+  beforeEach(function () {
+    var dataDir = path.resolve('spec/fixture/data');
+    data = new Data({dataDir: dataDir});
+  });
+
+  // describe('data.getTracks()', function () {
+
+  //   it('should load from data directory if load is not called first', function () {
+  //     var dataWithoutLoad = data.tracks();
+  //     data.load();
+  //     expect(data.tracks()).toEqual(dataWithoutLoad);
+  //   });
+
+  // });
+
 });
